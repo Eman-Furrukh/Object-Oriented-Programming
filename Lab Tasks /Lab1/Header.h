c9d9e0460edea7cca93d215aef555ddef74e15a9
Eman Furrukh <emanfurrukh09@gmail.com>
2024-07-13 21:18:57 +0500

    Create Header.h

---
+++ Lab Tasks /Lab1/Header.h
@@ -0,0 +1,17 @@
+#pragma once
+
+int sum(int a, int b, int c) {
+	return a + b + c;
+}
+
+int sub(int a, int b) {
+	return a-b;
+}
+
+int multi(int a, int b) {
+	return a * b;
+}
+
+int divide(int a, int b) {
+	return a / b;
+}
