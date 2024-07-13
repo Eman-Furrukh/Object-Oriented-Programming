b2147d118feb80653855d4a232bb33c85267d936
Eman Furrukh <emanfurrukh09@gmail.com>
2024-07-13 20:10:13 +0500

    Create q5.cpp

---
+++ Assignments/Ass#3 /Q5 /q5.cpp
@@ -0,0 +1,21 @@
+//============================================================================
+// Name        : q5.cpp
+// Author      : Eman Furrukh
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+#include <fstream>
+#include "question.h"
+using namespace std;
+
+int main()
+{
+	question q;
+	q.Player1();
+	q.Player2();
+	q.Result();
+	return 0;
+}
