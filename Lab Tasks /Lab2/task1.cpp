c0031148b2ed50997e0746739214ddb27d8a90f1
Eman Furrukh <emanfurrukh09@gmail.com>
2024-07-13 21:23:30 +0500

    Update task1.cpp

--- Lab Tasks /Lab2/task1.cpp
+++ Lab Tasks /Lab2/task1.cpp
@@ -1,17 +1,30 @@
-/*    TASK 2   */
+/*   TASK 1  */
 
 #include <iostream>
 using namespace std;
 
 int main() {
-	int* num;
-	num = new int;
-	cout << "Enter number to calculate factorial: ";
-	cin >> *num;
-	float fact = 1.0;
-	for (int i = 1; i <= *num; i++)
-		fact *= i;
-	cout << "Factorial of " << *num << " is " << fact << endl;
-	delete num;
+	float* ptr1, * ptr2;
+	ptr1 = new float;
+	ptr2 = new float;
+	cout << "Enter you marks: ";
+	cin >> *ptr2;
+
+	if (*ptr2 >= 91 && *ptr2 < 101)
+		cout << "Grade: A+ \n";
+	else if (*ptr2 >= 81 && *ptr2 <= 90)
+		cout << "Grade: A \n";
+	else if (*ptr2 >= 71 && *ptr2 <= 80)
+		cout << "Grade: B \n";
+	else if (*ptr2 >= 61 && *ptr2 <= 70)
+		cout << "Grade: C \n";
+	else if (*ptr2 >= 51 && *ptr2 <= 60)
+		cout << "Grade: D \n";
+	else if (*ptr2 <= 50 && *ptr2 >= 0)
+		cout << "Grade: FAIL \n";
+	else
+		cout << "INVALID INPUT.\n";
+
+	delete ptr2;
 	return 0;
 }
